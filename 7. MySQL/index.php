26153d0b0b902574cd7d9bf206859b9e8fd78d4a
oliverstaton <oliver.william.staton@gmail.com>
2014-10-26 23:40:58 -0400

    Learned retrieve, add, loop through data base elements

--- 7. MySQL/index.php
+++ 7. MySQL/index.php
@@ -9,34 +9,59 @@
 		die("ERROR: Could not connect to database");
 	}
 	
+	
+	
 	/*
 		Use INSERT to insert items into the data base
 	*/
-	//$query = "INSERT INTO `users` (`name`, `email`, `password`) VALUES('Eli', 'eli@gmail.com', 'password')";
+//	$query = "INSERT INTO `users` (`name`, `email`, `password`) VALUES('Eli', 'eli@gmail.com', 'password')";
+//	
+//	if($result = mysqli_query($link, $query)) {
+//		echo ".";
+//	}
+//	else {
+//		die("Error: Insert query did not work");
+//	}
 	
-	//if($result = mysqli_query($link, $query)) {
-	//	echo ".";
-	//}
-	//else {
-	//	die("Error: Insert query did not work");
-	//}
 	
-	$query = "UPDATE `users` SET `email`*'eli@hotmail.com'"
+	
+	/*
+		Use UPDATE to update items in the data base (WHERE this condition is true (LIMIT the number of updates)
+	*/
+//	$query = "UPDATE `users` SET `email`='eli@hotmail.com' WHERE `id`=2 LIMIT 1";
+//	$query = "UPDATE `users` SET `name`='Francis' WHERE `name`='Eli'";
+//	$query = "UPDATE `users` SET `name`='Tim O\'Connell' WHERE `id`=2 LIMIT 1";
+//	
+//	if($result = mysqli_query($link, $query)) {
+//		echo ".";
+//	}
+//	else {
+//		die("ERROR: Update query did not work");
+//	}
+	
+	
 	
 	/*
 		Use SELECT to select items from the data base
 	*/		
-	$query = "SELECT * FROM users";
+//	$name="Tim O'Connell";
 	
-	if($result = mysqli_query($link, $query)) {
-		
-		$row = mysqli_fetch_array($result);
-		
-		print_r($row);
-		
-	}
-	else {
-		die("ERROR: Select query did not work");
-	}
+//	$query = "SELECT * FROM users WHERE email LIKE '%gmail%'";
+//	$query = "SELECT `name` FROM `users` WHERE `name`='".mysqli_real_escape_string($link, $name)."'";
+//	
+//	if($result = mysqli_query($link, $query)) {
+//		
+//		// Determine number of rows in the data base category
+//		echo mysqli_num_rows($result);
+//		
+//		// Cycle through all valid rows
+//		while($row = mysqli_fetch_array($result))
+//		{
+//			print_r($row);		
+//		}
+//	}
+//	else {
+//		die("ERROR: Select query did not work");
+//	}
 
 ?>
